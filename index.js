dc6cdd8cd3adb8e95553aae5036193ed1c4d9ee6
hugh-vincent <138526140+hugh-vincent@users.noreply.github.com>
2023-07-04 00:06:45 +0100

    Update index.js
    Removed the failed bw and blue changer

--- index.js
+++ index.js
@@ -1,61 +1,9 @@
 const poems = [
-  `Action Man arrived
-in a lunar rover
-dressed as an astronaut.
-We turned the living room rug
-into the red valleys of Mars
-and drove all around.
-I never could have been an astronaut -
-the idea of space
-terrifies me.
-One slip and you’re lost forever.
-Punch your way out of that one,
-Action Man.`,
-
-  
-  `I spent all of my
-pocket money
-on plastic swords.
-They transported me
-as I walked down
-the hot, sandy
-pavement.
-I felt so powerful-
-I never wanted to be
-born here.
-I wanted to be a knight,
-a warrior, I even said so!
-But looking back,
-it all seems so cosy
-with words like
-‘Turbo’, ‘Max’ and ‘Ultra’
-everywhere.`,
-
-
-  
-  `I couldn’t believe
-my luck
-When Dad gave me
-his old Apple Mac.
-There was no internet,
-there were no games.
-Just me,
-in the reflection.
-I sat there in my
-little green room.
-Underneath the dormer window
-pretending to be a writer.
-Clack clack
-clack.
-Typing away
-at my very own
-Apple Mac.`,
-
-  
+  `pom1`,
+  `pom2`,
+  `pom3`,
   // Add more poems as needed
-
-  
-];
+  ];
 
 let isLoadingImage = false;
 
@@ -101,24 +49,6 @@ function applyImageEffects(image, canvas) {
     width,
     height
   );
-
-  // Convert the image to black and white
-  const imageData = ctx.getImageData(0, 0, width, height);
-  const data = imageData.data;
-  for (let i = 0; i < data.length; i += 4) {
-    const r = data[i];
-    const g = data[i + 1];
-    const b = data[i + 2];
-    const gray = 0.2989 * r + 0.587 * g + 0.114 * b;
-    data[i] = data[i + 1] = data[i + 2] = gray;
-  }
-  ctx.putImageData(imageData, 0, 0);
-
-  // Apply blue color overlay
-  ctx.globalCompositeOperation = 'color';
-  ctx.fillStyle = 'rgba(0, 0, 255, 0.7)';
-  ctx.fillRect(0, 0, width, height);
-  ctx.globalCompositeOperation = 'source-over';
 }
 
 function loadRandomPoem() {
